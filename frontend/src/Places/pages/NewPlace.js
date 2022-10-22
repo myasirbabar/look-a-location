95719134be68a6c9130aab42d1b38afd14c6ee69
Muhammad Yasir <ybabar1@gmail.com>
2022-10-22 12:02:40 +0500

    Input Component Added

--- frontend/src/Places/pages/NewPlace.js
+++ frontend/src/Places/pages/NewPlace.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Input from '../../Shared/components/FormElements/Input'
+import './NewPlace.css'
 
 const NewPlace = () => {
   return (
-    <h1>New Place Page</h1>
+    <form className='place-form'>
+      <Input element = "input" type = "text" placeholder = "Enter Title" label = "Title" />
+    </form>
   )
 }
 
