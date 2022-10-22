ed4d72d32645a355d7a2df03c3f75172a525b688
Muhammad Yasir <ybabar1@gmail.com>
2022-10-22 13:36:10 +0500

    State Management In Input Component

--- frontend/src/Places/pages/NewPlace.js
+++ frontend/src/Places/pages/NewPlace.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import Input from '../../Shared/components/FormElements/Input'
-import './NewPlace.css'
+import React from "react";
+import Input from "../../Shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
-    <form className='place-form'>
-      <Input element = "input" type = "text" placeholder = "Enter Title" label = "Title" />
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        placeholder="Enter Title"
+        label="Title"
+        validators = {[]}
+        errorText = "Please Enter a Valid Title"
+      />
     </form>
-  )
-}
+  );
+};
 
 export default NewPlace;
--- frontend/src/Shared/components/FormElements/Input.js
+++ frontend/src/Shared/components/FormElements/Input.js
@@ -1,21 +1,59 @@
-import React from 'react'
-import './Input.css'
+import React from "react";
+import { useReducer } from "react";
+import "./Input.css";
 
-const Input = props => {
-    const element = props.element === "input" ?
-        (
-            <input id={props.id} type={props.type} placeholder= {props.placeholder}></input>
-        )
-        : (
-            <textarea id={props.id} rows={props.rows || 3}></textarea>
-        )
-    return (
-        <div className='form-control'>
-            <label htmlFor={props.id}>{props.label}</label>
-            {element}
-        </div>
-    )
-}
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: true,
+      };
+    default:
+      return state;
+  }
+};
 
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+  });
 
-export default Input
+  const inputChangeHandler = (event) => {
+    dispatch({ type: "CHANGE", val: event.target.value });
+  };
+
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={inputChangeHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={inputChangeHandler}
+        value={inputState.value}
+      ></textarea>
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && <p> {props.errorText} </p>}
+    </div>
+  );
+};
+
+export default Input;
