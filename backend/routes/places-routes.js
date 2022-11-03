cbdfb238da1d5e93c845900b9b93ce2980f62340
Muhammad Yasir <ybabar1@gmail.com>
2022-11-03 23:34:07 +0500

    Controllers Added

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,64 +1,11 @@
 const express = require("express");
-const HttpError = require("../models/http-error");
 const router = express.Router();
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    creator: "u1",
-    title: "Faculty Of Computing and Inforation Technology - NC",
-    description:
-      "Offers a diverse knowledge in Computer Science and Related Fields",
-    imageURL: "https://pucit.edu.pk/wp-content/uploads/2021/09/dap9_n-1.jpg",
-    address:
-      "F7J8+53W, Samsani Road, Quaid-i-Azam Campus, لاہور, Lahore, پنجاب",
-    location: {
-      lat: 31.4804833,
-      lng: 74.2630103,
-    },
-  },
-  {
-    id: "p2",
-    creator: "u2",
-    title: "Faculty Of Computing and Inforation Technology - OC",
-    description:
-      "Offers a diverse knowledge in Computer Science and Related Fields",
-    imageURL:
-      "https://dailytimes.com.pk/assets/uploads/2022/07/29/2560px-Punjab_University-768x432.jpg",
-    address: "Katchery Road، Near Anarkali Bazar، Lahore, 54000, Pakistan",
-    location: {
-      lat: 31.5703641,
-      lng: 74.3074407,
-    },
-  },
-];
+const placesController = require("../controllers/places-controller")
 
 // Get Place
-router.get("/:pid", (req, res, next) => {
-  console.log("Get Places Request In Places");
-
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => p.id === placeId);
-
-  if (!place) {
-    throw new HttpError("Could not Find Place with this Id", 404); 
-  }
-
-  res.json({ place });
-});
+router.get("/:pid", placesController.getPlaceById);
 
 // Get User Places
-router.get("/user/:uid", (req, res, next) => {
-  console.log("Get User Places Request In Places");
-
-  const userID = req.params.uid;
-  const places = DUMMY_PLACES.find((p) => {
-    return p.creator === userID;
-  });
-  if (!places) {
-    return next(new HttpError("Could Not Find Any Places for this user", 404));
-  }
-  res.json({ places });
-});
+router.get("/user/:uid", placesController.getPlaceByUserId);
 
 module.exports = router;
