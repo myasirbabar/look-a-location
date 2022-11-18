190814e9b7444d8c94cc271cf6893151cf12b83d
Muhammad Yasir <ybabar1@gmail.com>
2022-11-18 10:06:53 +0500

    Mongoose Schema Added For Places

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,47 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const HttpError = require("./models/http-error")
-const placesRoutes = require('./routes/places-routes')
-const usersRoutes = require('./routes/users-routes')
+const mongoose = require("mongoose");
+const HttpError = require("./models/http-error");
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 
 const app = express();
 const port = 5000;
 
 // Body Parser
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Register MiddleWare For Places
-app.use('/api/places',placesRoutes); // Places
+app.use("/api/places", placesRoutes); // Places
 
 // Register MiddleWare For Places
-app.use('/api/users',usersRoutes); // Places
-
+app.use("/api/users", usersRoutes); // Places
 
 // Register MiddleWare For Unknown Routes
-app.use((req,res,next)=>{
-
-  const error = new HttpError('Could Not Find This Route ', 404);
+app.use((req, res, next) => {
+  const error = new HttpError("Could Not Find This Route ", 404);
   throw error;
-  
-})
-
+});
 
 // Error Middleware
-app.use((error, req,res,next)=>{
-  if(res.headerSent){
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
     return next(error);
   }
-  
-  res.status(error.code || 500)
-  res.json({message: error.message || "Unknown Error Occured"});
-})
 
+  res.status(error.code || 500);
+  res.json({ message: error.message || "Unknown Error Occured" });
+});
 
-// INIT SERVER
-app.listen(port, () =>
-  console.log(`Look a Location app listening on port ${port}!`)
-);
+// If Database Connection is successfull start backend server
+mongoose
+  .connect("mongodb+srv://myb:IVcrP8z4PX2a42Q4@cluster0.ossa8dp.mongodb.net/places?retryWrites=true&w=majority")
+  .then(() => {
+    // INIT SERVER
+    app.listen(port, () =>
+      console.log(`Look a Location app listening on port ${port}!`)
+    );
+  })
+  .catch(err =>{
+    throw new HttpError(err, 404);
+  });
