e6a14af6339a9c85d111e4e050c739e32ade3aa1
Muhammad Yasir <ybabar1@gmail.com>
2022-11-26 11:52:30 +0500

    Handling Images in backend using multer

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const usersController = require("../controllers/users-controllers");
-
+const fileUpload = require('../middleware/file-upload');
 // Get All Users
 router.get("/", usersController.getUsers);
 
 // Signup
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
